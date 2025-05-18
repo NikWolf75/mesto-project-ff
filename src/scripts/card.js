79bcb1f4dc213a10afd50f7719118f7e0f421152
Nikita Kyulyuev <punkusha75@gmail.com>
2025-05-19 01:12:38 +0300

    Проектная работа fix3

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,22 +1,3 @@
-export function openPopup(popup) {
-  popup.classList.add("popup_is-opened");
-  document.addEventListener("keydown", handleEscClose);
-}
-
-export function closePopup(popup) {
-  popup.classList.remove("popup_is-opened");
-  document.removeEventListener("keydown", handleEscClose);
-}
-
-export function handleEscClose(event) {
-  if (event.key === "Escape") {
-    const openedPopup = document.querySelector(".popup_is-opened");
-    if (openedPopup) {
-      closePopup(openedPopup);
-    }
-  }
-}
-
 export function toggleLike(event) {
   event.target.classList.toggle("card__like-button_is-active");
 }
