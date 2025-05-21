80b135b9f9c6d2900407ee6bae5582e462d339af
Nikita Kyulyuev <punkusha75@gmail.com>
2025-05-21 19:57:56 +0300

    Update12

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,7 +1,10 @@
-const cohortId = "wff-cohort-39";
-const token = "ba26a2bd-21bb-4faa-8017-2c8d6bacad83";
-
-const baseUrl = `https://nomoreparties.co/v1/${cohortId}`;
+const config = {
+  baseUrl: 'https://nomoreparties.co/v1/wff-cohort-39',
+  headers: {
+    authorization: 'ba26a2bd-21bb-4faa-8017-2c8d6bacad83',
+    'Content-Type': 'application/json'
+  }
+};
 
 function checkResponse(res) {
   if (res.ok) {
@@ -10,79 +13,67 @@ function checkResponse(res) {
   return Promise.reject(`Ошибка: ${res.status}`);
 }
 
-function getUserInfo() {
-  return fetch(`${baseUrl}/users/me`, {
-    headers: { authorization: token },
-  }).then(checkResponse);
-}
+export const getUserInfo = () => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    headers: config.headers
+  })
+  .then(checkResponse);
+};
 
-function getInitialCards() {
-  return fetch(`${baseUrl}/cards`, {
-    headers: { authorization: token },
-  }).then(checkResponse);
-}
+export const getInitialCards = () => {
+  return fetch(`${config.baseUrl}/cards`, {
+    headers: config.headers
+  })
+  .then(checkResponse);
+};
 
-function updateUserInfo({ name, about }) {
-  return fetch(`${baseUrl}/users/me`, {
+export const updateUserInfo = ({ name, about }) => {
+  return fetch(`${config.baseUrl}/users/me`, {
     method: "PATCH",
-    headers: {
-      authorization: token,
-      "Content-Type": "application/json",
-    },
+    headers: config.headers,
     body: JSON.stringify({ name, about }),
-  }).then(checkResponse);
-}
+  })
+  .then(checkResponse);
+};
 
-function addNewCard({ name, link }) {
-  return fetch(`${baseUrl}/cards`, {
+export const addNewCard = ({ name, link }) => {
+  return fetch(`${config.baseUrl}/cards`, {
     method: "POST",
-    headers: {
-      authorization: token,
-      "Content-Type": "application/json",
-    },
+    headers: config.headers,
     body: JSON.stringify({ name, link }),
-  }).then(checkResponse);
-}
+  })
+  .then(checkResponse);
+};
 
-function likeCard(cardId) {
-  return fetch(`${baseUrl}/cards/likes/${cardId}`, {
+export const likeCard = (cardId) => {
+  return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
     method: "PUT",
-    headers: { authorization: token },
-  }).then(checkResponse);
-}
+    headers: config.headers,
+  })
+  .then(checkResponse);
+};
 
-function unlikeCard(cardId) {
-  return fetch(`${baseUrl}/cards/likes/${cardId}`, {
+export const unlikeCard = (cardId) => {
+  return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
     method: "DELETE",
-    headers: { authorization: token },
-  }).then(checkResponse);
-}
+    headers: config.headers,
+  })
+  .then(checkResponse);
+};
 
-function deleteCardFromServer(cardId) {
-  return fetch(`${baseUrl}/cards/${cardId}`, {
+export const deleteCardFromServer = (cardId) => {
+  return fetch(`${config.baseUrl}/cards/${cardId}`, {
     method: "DELETE",
-    headers: { authorization: token },
-  }).then(checkResponse);
-}
+    headers: config.headers,
+  })
+  .then(checkResponse);
+};
 
-function updateAvatar(avatarUrl) {
-  return fetch(`${baseUrl}/users/me/avatar`, {
+export const updateAvatar = (avatarUrl) => {
+  return fetch(`${config.baseUrl}/users/me/avatar`, {
     method: "PATCH",
-    headers: {
-      authorization: token,
-      "Content-Type": "application/json",
-    },
+    headers: config.headers,
     body: JSON.stringify({ avatar: avatarUrl }),
-  }).then(checkResponse);
-}
-
-export {
-  getUserInfo,
-  getInitialCards,
-  updateUserInfo,
-  addNewCard,
-  likeCard,
-  unlikeCard,
-  deleteCardFromServer,
-  updateAvatar
+  })
+  .then(checkResponse);
 };
